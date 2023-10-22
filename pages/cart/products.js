751ac296a5b68292091f79b66ed12086e86e9014
Karcst <106001805+Kartosz@users.noreply.github.com>
2023-10-22 09:50:01 +0700

    a

--- pages/cart/products.js
+++ pages/cart/products.js
@@ -8,31 +8,31 @@ export const products = [
     {
         "id": 2,
         "name": "Stray",
-        "price": 15,
+        "price": 30,
         "image": "../../img/trending2.jpg"
     },
     {
         "id": 3,
         "name": "Little Nightmares 2",
-        "price": 15,
+        "price": 29.99,
         "image": "../../img/trending3.jpg"
     },
     {
         "id": 4,
         "name": "Doom: Eternal",
-        "price": 15,
+        "price": 39.50,
         "image": "../../img/trending4.jpg"
     },
     {
         "id": 5,
         "name": "Poppy PLaytime",
-        "price": 15,
+        "price": 9.80,
         "image": "../../img/trending5.jpg"
     },
     {
         "id": 6,
         "name": "Dying Light 2",
-        "price": 15,
+        "price": 59.45,
         "image": "../../img/trending6.jpg"
     },
     {
@@ -44,7 +44,19 @@ export const products = [
     {
         "id": 8,
         "name": "Resident Evil: Village",
-        "price": 15,
+        "price": 48.30,
         "image": "../../img/trending8.jpg"
-    }
+    },
+    {
+        "id": 9,
+        "name": "Alien: Isolation",
+        "price": 16.9,
+        "image": "../../img/trending9.jpg"
+    },
+    {
+        "id": 10,
+        "name": "Dead Space",
+        "price": 27.4,
+        "image": "../../img/trending10.jpg"
+    },
 ];
